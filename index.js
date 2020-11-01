8dda45bdea6973dddb5d610ec29895b498e45d0c
jsingle345 <jsingle345@gmail.com>
2020-11-01 10:54:05 -0600

    commit tree 9426ac1c193d84b739b15d458cecf9838a255043

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+
+const main = document.querySelector('#main');
+const newHeader = document.createElement('h1');
+
+main.remove();
+
+newHeader.setAttribute("id", "victory")
+newHeader.innerHTML = "Joshua is the champion"
+
+document.body.append(newHeader)
